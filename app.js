bd3252c908e566ee7469d0862e2ac7b3505db4ad
Catriece Gilbert <catriece.gilbert@gmail.com>
2023-01-03 17:21:58 -0500

    JavaScript Variable Exercise

--- app.js
+++ app.js
@@ -5,6 +5,40 @@ console.log(
 
 // Exercise 1
 
+const firstName = "Catriece";
+let lastName = "Gilbert";
+var age = 27;
+
 // Exercise 2
 
+lastName = "Gilbert";
+age = 27;
+
 // Exercise 3
+
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+console.log(`${language} was first released in ${createdYear}.`);
+
+// Exercise 4
+
+let product = "Dress"
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15; 
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(`The ${product} in size ${selectedSize} is $${price} with ${stock} in stock.`)
+
+//Exercise 5
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`I am on page ${bookmark} of ${pageCount} in the book titled ${title} by ${author}.`)
